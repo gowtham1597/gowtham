b053595f9ac55e0dec78ba69d16eff9518a16e55
gowtham1597 <30342619+gowtham1597@users.noreply.github.com>
2017-08-05 04:22:27 -0700

    Create gow.c

---
+++ gow.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main()
+{
+  char cha;
+ 
+  printf("Enter a character\n");
+  scanf("%c", &cha);
+ 
+  if (cha == 'a' || cha == 'A' || cha == 'e' || cha == 'E' || cha == 'i' || cha == 'I' || cha =='o' || cha =='O' || cha == 'u' || cha == 'U')
+    printf("%c is a vowel.\n", cha);
+  else
+    printf("%c is not a vowel.\n", cha);
+ 
+  return 0;
