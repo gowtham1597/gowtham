d5b2a54d09430ed69677e6f2e58b55af2eb8f079
gowtham1597 <30342619+gowtham1597@users.noreply.github.com>
2017-08-05 04:03:26 -0700

    Update red.c

--- red.c
+++ red.c
@@ -4,11 +4,11 @@ int main()
     int a;
 
     printf("Enter an integer: ");
-    scanf("%d", &a);
+    scanf("%d",&a);
     if(a % 2 == 0)
-        printf("%d is even.", a);
+        printf("%d is even", a);
     else
-        printf("%d is odd.", a);
+        printf("%d is odd", a);
 
     return 0;
 }
