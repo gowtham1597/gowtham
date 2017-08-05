de04aecc601972f166ae79cdeebd78f8d8541e5d
gowtham1597 <30342619+gowtham1597@users.noreply.github.com>
2017-08-05 04:26:54 -0700

    Update gow.c

--- gow.c
+++ gow.c
@@ -8,8 +8,10 @@ int main()
   scanf("%c", &cha);
  
   if (cha == 'a' || cha == 'A' || cha == 'e' || cha == 'E' || cha == 'i' || cha == 'I' || cha =='o' || cha =='O' || cha == 'u' || cha == 'U')
+  {
     printf("%c is a vowel.\n", cha);
   else
     printf("%c is not a vowel.\n", cha);
- 
+  }
   return 0;
+}
