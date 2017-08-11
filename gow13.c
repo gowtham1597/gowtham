caa88b6daccf610142c5a22e809f276f3c283791
gowtham1597 <30342619+gowtham1597@users.noreply.github.com>
2017-08-11 04:18:27 -0700

    Create gow13.c

---
+++ gow13.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int a, i, flag = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d",&a);
+
+    for(i=2; i<=a/2; ++i)
+    {
+        // condition for nonprime number
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    if (flag==0)
+        printf("%d is a prime number.",n);
+    else
+        printf("%d is not a prime number.",n);
+    
+    return 0;
+}
